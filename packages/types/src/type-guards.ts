fdc6a37e3f79e7594209071d36f46e1f76611ed1
Felipe César <felipecesr@gmail.com>
2020-10-17 18:18:00 -0300

    feat(types): add sum function

--- packages/types/src/type-guards.ts
+++ packages/types/src/type-guards.ts
@@ -67,3 +67,16 @@ export function isMessage(arg: any): arg is IMessage {
     typeof arg.body === "string"
   );
 }
+
+/**
+ * Calculate the sum of two numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @internal
+ */
+export function sum(a: number, b: number): number {
+  const sum = a + b;
+  return sum;
+}
